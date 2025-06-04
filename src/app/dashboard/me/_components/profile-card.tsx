b4d80c95dc852bd397540e140b85f8bd38826308
Deda <louanderson7@hotmail.com>
2025-06-03 21:23:41 -0300

    feat: add ProfileCard component for displaying user profile with image, name, and bio

---
+++ src/app/dashboard/me/_components/profile-card.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import { Name } from "./name";
+import { Description } from "./description";
+
+interface ProfileCardProps {
+  user: {
+    id: string;
+    name: string | null;
+    username: string | null;
+    bio: string | null;
+    image: string | null;
+  };
+}
+
+export function ProfileCard({ user }: ProfileCardProps) {
+  return (
+    <section className="w-full flex flex-col items-center mx-auto px-4">
+      <div className="">
+        <Image
+          src={user.image ?? "/images/default-profile.png"}
+          alt="Foto de perfil"
+          width={104}
+          height={104}
+          className="rounded-xl bg-gray-50 object-cover border-4 border-white hover:shadow-xl duration-300"
+          priority
+          quality={100}
+        />
+      </div>
+
+      <div>
+        <Name initialName={user.name ?? "Digite seu nome..."} />
+        <Description
+          initialDescription={user.bio ?? "Digite sua biografia..."}
+        />
+      </div>
+    </section>
+  );
+}
