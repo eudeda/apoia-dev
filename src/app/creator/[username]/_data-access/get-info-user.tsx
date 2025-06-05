7bb77b4773a5fc2abdeb9ad529f2887632f4d61a
Deda <louanderson7@hotmail.com>
2025-06-04 23:00:23 -0300

    feat: implement getInfoUser function for retrieving user information by username with validation

---
+++ src/app/creator/[username]/_data-access/get-info-user.tsx
@@ -0,0 +1,37 @@
+"use server";
+
+import { z } from "zod";
+import { prisma } from "@/lib/prisma";
+
+const createUsernameSchema = z.object({
+  username: z.string({ message: "O nome de usuário é obrigatório" }),
+});
+
+type CreateUsernameSchema = z.infer<typeof createUsernameSchema>;
+
+export async function getInfoUser(data: CreateUsernameSchema) {
+  const schema = createUsernameSchema.safeParse(data);
+
+  if (!schema.success) {
+    return null;
+  }
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: data.username,
+      },
+      select: {
+        id: true,
+        name: true,
+        username: true,
+        bio: true,
+        image: true,
+      },
+    });
+
+    return user;
+  } catch (err) {
+    return null;
+  }
+}
