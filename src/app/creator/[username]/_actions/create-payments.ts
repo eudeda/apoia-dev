f415e6d6fff2126c85300d799dea430b2788a1e9
Deda <louanderson7@hotmail.com>
2025-06-12 18:05:57 -0300

    feat: integrate Stripe checkout session creation in createPayment function

--- src/app/creator/[username]/_actions/create-payments.ts
+++ src/app/creator/[username]/_actions/create-payments.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { prisma } from "@/lib/prisma";
+import { stripe } from "@/lib/stripe";
 import { z } from "zod";
 
 const createUsernameSchema = z.object({
@@ -23,12 +24,77 @@ export async function createPayment(data: CreatePaymentSchema) {
     };
   }
 
+  if (!data.creatorId) {
+    return {
+      data: null,
+      error: "Falha ao criar o pagamento, tente mais tarde 1",
+    };
+  }
+
   try {
-    console.log(data);
+    const creator = await prisma.user.findFirst({
+      where: {
+        connectedStripeAccountId: data.creatorId,
+      },
+    });
+
+    if (!creator) {
+      return {
+        data: null,
+        error: "Falha ao criar o pagamento, tente mais tarde 2",
+      };
+    }
+
+    const applicationFeeAmount = Math.floor(data.price * 0.1);
+
+    const donation = await prisma.donation.create({
+      data: {
+        donorName: data.name,
+        donorMessage: data.message,
+        userId: creator.id,
+        status: "PENDING",
+        amount: data.price - applicationFeeAmount,
+      },
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      success_url: `${process.env.HOST_URL}/creator/${data.slug}`,
+      cancel_url: `${process.env.HOST_URL}/creator/${data.slug}`,
+      line_items: [
+        {
+          price_data: {
+            currency: "brl",
+            product_data: {
+              name: "Apoiar " + creator.name,
+            },
+            unit_amount: data.price,
+          },
+          quantity: 1,
+        },
+      ],
+      payment_intent_data: {
+        application_fee_amount: applicationFeeAmount,
+        transfer_data: {
+          destination: creator.connectedStripeAccountId as string,
+        },
+        metadata: {
+          donorName: data.name,
+          donorMessage: data.message,
+          donationId: donation.id,
+        },
+      },
+    });
+
+    return {
+      data: JSON.stringify(session),
+      error: null,
+    };
   } catch (err) {
     return {
       data: null,
-      error: "Falha ao criar o pagamento, tente mais tarde",
+      error: "Falha ao criar o pagamento, tente mais tarde 3",
     };
   }
 }
