f542154f5f578f970ee073a017dbd2e03b8a3a55
Deda <louanderson7@hotmail.com>
2025-06-12 18:06:37 -0300

    feat: add CreateAccountButton component for Stripe account creation

---
+++ src/app/dashboard/_components/create-account-button.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { toast } from "sonner";
+
+export function CreateAccountButton() {
+  const [loading, setloading] = useState(false);
+
+  async function handleCreateStripeAccount() {
+    setloading(true);
+
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_HOST_URL}/api/stripe/create-account`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        toast.error("Falha ao criar conta de pagamento");
+        setloading(false);
+        return;
+      }
+
+      window.location.href = data.url;
+    } catch (err) {
+      console.log(err);
+      setloading(false);
+    }
+  }
+
+  return (
+    <div className="mb-5">
+      <Button
+        className="cursor pointer"
+        onClick={handleCreateStripeAccount}
+        disabled={loading}
+      >
+        {loading ? "Carregando..." : "Ativar conta de pagamento"}
+      </Button>
+    </div>
+  );
+}
