3766f383e2dd0440b71ecf7e9be78f59e2e121fc
Deda <louanderson7@hotmail.com>
2025-06-26 23:00:30 -0300

    feat: update Stats component to fetch and display user statistics dynamically

--- src/app/dashboard/_components/analytics.tsx
+++ src/app/dashboard/_components/analytics.tsx
@@ -1,33 +1,42 @@
-
 import { Users, DollarSign, Wallet } from "lucide-react";
 import { StatCard } from "./stats-card";
+import { getStats } from "../_data-access/get-stats-creator";
+import { formatCurrency } from "@/utils/format";
 
-export async function Stats() {
+export async function Stats({
+  userId,
+  stripeAcountId,
+}: {
+  userId: string;
+  stripeAcountId: string;
+}) {
+  const { balance, totalAmountResult, totalQtyDonations } = await getStats(
+    userId,
+    stripeAcountId
+  );
 
   return (
     <div className="grid gap-6 grid-cols-1 md:grid-cols-3 mb-6">
-
       <StatCard
         title="Apoiadores"
         description="Total de apoiadores"
         icon={<Users className="w-8 h-8 text-blue-400" />}
-        value={0}
+        value={totalQtyDonations ?? 0}
       />
 
       <StatCard
         title="Total recebido"
         description="Quantidade total recebida"
         icon={<DollarSign className="w-8 h-8 text-amber-500" />}
-        value={0}
+        value={formatCurrency((totalAmountResult ?? 0) / 100)}
       />
 
       <StatCard
         title="Saldo em conta"
         description="Saldo pendente"
         icon={<Wallet className="w-8 h-8 text-green-500" />}
-        value={0}
+        value={formatCurrency((balance ?? 0) / 100)}
       />
-
     </div>
   );
 }
